871dea7d4b7d030b4f8ea556c9ef8ce34b127e6a
ErikaBraithe <erikabraithwaite@gmail.com>
2018-03-11 19:53:45 -0400

    added data

--- joining.R
+++ joining.R
@@ -5,13 +5,22 @@ library(pacman)
 
 pacman::p_load(data.table, dplyr, tinytex, knitr)
 
+# Scenario 1 
 day1 =  data.table(ID=LETTERS[1:12],
-                 y=round(rnorm(4),4))
+                 y1=round(rnorm(4),4))
 
-df =  data.table(ID=LETTERS[1:12],
-                 V3=round(rnorm(4),4),
-                 V4=1:12)
+day2 =  data.table(ID=LETTERS[6:17],
+                 y2=round(rnorm(6),4))
 
-&&
+day1
+day2
+
+left = left_join(day1, day2)
+right = right_join(day1, day2)
+inner= inner_join(day1, day2)
+full = full_join(day1, day2)
+semi = semi_join(day1, day2)
+anti = anti_join(day1, day2)
+
         
 
